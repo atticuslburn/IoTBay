bd40a390916aebb51e6546e0df589a08c9985e66
NimaNadaliJelokhani <Nima.NadaliJelokhani@student.uts.edu.au>
2025-03-26 19:17:30 +1100

    making java classes

---
+++ src/main/java/Delivery.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Delivery {
+    private int deliveryID;
+    private Date deliveryDate;
+    private int orderID;
+    private String deliveryReciever;
+}
---
+++ src/main/java/Order.java
@@ -0,0 +1,7 @@
+import java.util.Date;
+
+public class Order {
+    private int orderID;
+    private Date orderDate;
+    private int itemID;
+}
