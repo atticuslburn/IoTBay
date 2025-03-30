7db31c512e919cab7c7c75b1082c82e56f4143e4
AdrianHackerMaster <110707510+AdrianHackerMaster@users.noreply.github.com>
2025-03-30 18:56:41 +1100

    Update User.java

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,10 +1,129 @@
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
+
     private int userID;
+    private int userTypeID;
     private String firstName;
     private String lastName;
-    private String email;
-    private String phone;
-    private int StreetNumber;
+    private int phoneNumber;
+    private int streetNumber;
+    private String streetName;
     private String suburb;
-    private String postcode;
+    private int regionID;
+    private int postcode;
+    private String email;
+    private String userPassword;
+
+    public User() {}
+
+    public User(int userID, int userTypeID, String firstName, String lastName, int phoneNumber, int streetNumber, String streetName, String suburb, int regionID, int postcode, String email, String userPassword) {
+        this.userID = userID;
+        this.userTypeID = userTypeID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.streetNumber = streetNumber;
+        this.streetName = streetName;
+        this.suburb = suburb;
+        this.regionID = regionID;
+        this.postcode = postcode;
+        this.email = email;
+        this.userPassword = userPassword;
+    }
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public int getUserTypeID() {
+        return userTypeID;
+    }
+
+    public void setUserTypeID(int userTypeID) {
+        this.userTypeID = userTypeID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(int streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public String getSuburb() {
+        return suburb;
+    }
+
+    public void setSuburb(String suburb) {
+        this.suburb = suburb;
+    }
+
+    public int getRegionID() {
+        return regionID;
+    }
+
+    public void setRegionID(int regionID) {
+        this.regionID = regionID;
+    }
+
+    public int getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode(int postcode) {
+        this.postcode = postcode;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
 }
